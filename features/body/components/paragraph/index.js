c6053d740d62ba18bcbd885aa6f05e51f429b11a
Josh Kadis <josh.kadis@wsj.com>
2020-10-01 13:42:09 -0400

    bold specific paragraphs

--- features/body/components/paragraph/index.js
+++ features/body/components/paragraph/index.js
@@ -1,11 +1,22 @@
 import renderCAPI from 'helpers/renderCAPI';
 import styles from './styles.module.css';
 
+const textsForBold = [
+  'Mafalda was a 6-year old schoolgirl',
+  'Joaquín Salvador Lavado, better known by his nickname Quino',
+  '“I don’t think the problem is that political systems',
+  'Following Argentina’s military coup',
+].map((text) => text.slice(0, 20));
+
+const paragraphIsBold = (pg) => textsForBold.includes(pg.slice(0, 20));
+
 const Paragraph = props => {
   const { data: { content: paragraphContent } } = props;
   const content = renderCAPI(paragraphContent);
+  const isBold = paragraphIsBold(content);
+  const className = `${styles.paragraph} ${isBold ? styles['is-bold'] : ''}`;
   return (
-    <p className={styles.paragraph} dangerouslySetInnerHTML={{ __html: content }}/>
+    <p className={className} dangerouslySetInnerHTML={{ __html: content }} />
   );
 };
 
