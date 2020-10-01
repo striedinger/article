9f135a60c00e91b44b14af3b1ab2a38b9c187ba2
Brian Whitton <brian.whitton@wsj.com>
2020-10-01 15:01:54 -0400

    if it's a highlighWithTooltip graf, render a highlighted tooltip

--- features/body/components/paragraph/index.js
+++ features/body/components/paragraph/index.js
@@ -1,18 +1,26 @@
 import renderCAPI from 'helpers/renderCAPI';
 import styles from './styles.module.css';
+import Info from '../../../../components/svg/info.svg';
 
 const Paragraph = props => {
-  const { data: { content: paragraphContent, highlight } } = props;
+  const { data: { content: paragraphContent, highlight, highlightWithTooltip } } = props;
   const content = renderCAPI(paragraphContent);
-  if (highlight) {
+  if (highlight || highlightWithTooltip) {
     return (
       <p className={styles.paragraph}>
         <span className={styles.highlight}>
           {content}
+          {
+            highlightWithTooltip && 
+            <span className={styles['info-icon']}>
+              <Info/>
+            </span>
+          }
         </span>
       </p>
     );
   }
+
   return (
     <p className={styles.paragraph}>{content}</p>
   );
